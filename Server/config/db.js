f9ad931b912528630a9aca3b2577d496f70908d1
Keith Kirtfield <keithkirtfield@Keiths-MBP.fios-router.home>
2021-01-09 18:52:48 -0500

    added k8s confog files

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+
 const connectdb = async () => {
-  const connect = await mongoose.connect(process.env.MONGO_KEY, {
+  const connect = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
--- Server/index.js
+++ Server/index.js
@@ -9,6 +9,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const hpp = require('hpp');
+const mongoDb = require('./config/db');
 dotenv.config({ path: './config/config.env' });
 const routes = require('./routes/routes');
 const courseRoutes = require('./routes/courses');
@@ -16,9 +17,7 @@ const authROute = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
 const reviewRoutes = require('./routes/review');
 const morgan = require('morgan');
-const mongoDb = require('./config/db');
 const errorHandler = require('./middleware/error');
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(sanitize());
@@ -38,11 +37,11 @@ require('./models/course');
 app.use(fileUpload());
 // Set Static foldure
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/v1/bootcamps', routes);
-app.use('/api/v1/courses', courseRoutes);
-app.use('/api/v1/auth', authROute);
-app.use('/api/v1/admin', adminRoutes);
-app.use('/api/v1/reviews', reviewRoutes);
+app.use('/v1/bootcamps', routes);
+app.use('/v1/courses', courseRoutes);
+app.use('/v1/auth', authROute);
+app.use('/v1/admin', adminRoutes);
+app.use('/v1/reviews', reviewRoutes);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
