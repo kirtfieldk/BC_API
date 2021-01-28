46fb0c33e72be13988c5a306d9e401ec49e25207
Keith Kirtfield <keithkirtfield@Keiths-MacBook-Pro.local>
2021-01-28 18:38:22 -0500

    testing flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Routes from "./routes/Routes"
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5000'
+// axios.defaults.baseURL = 'http://localhost:5000'
 function App() {
   return <div className="App"><Routes/></div>;
 }
